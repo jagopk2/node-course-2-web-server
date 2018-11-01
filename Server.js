cf42ec7548c1c69c5636e20dff376faf577a11bf
Your Name <you@example.com>
2018-11-01 18:48:38 +0500

    2nd push

---
+++ Server.js
@@ -0,0 +1,32 @@
+ const hbs = require('hbs');
+ const express = require('express');
+ var app = express();
+
+ app.set('view engine','hbs');
+ app.use(express.static(__dirname + '/public'));
+// app.use((req,res,next) => {
+//       res.render('home.hbs',{
+//         PageTitle : 'Maintainence Page',
+//         currentYear : new Date().getFullYear()
+//       });
+     
+// });
+
+
+ app.get('/', (req, res) => {
+    //  res.send('<h1> This is the Home Page </h1>');
+    res.render('home.hbs',{
+        PageTitle : 'Home',
+        currentYear : new Date().getFullYear()
+    });
+ });
+
+ app.get('/About', (req, res) => {
+    res.render('home.hbs',{
+        PageTitle : 'About Page',
+        currentYear : new Date().getFullYear()
+    });
+ });
+
+
+ app.listen(3000);
